6db7c08e8efb35ad3c9932b4640f59d50aec258c
Dafondo <austin.wang0311@gmail.com>
2017-11-04 07:33:39 -0400

    revert to nonlive

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -15,29 +15,19 @@ var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 $(document).ready(function() {
     $('.scrollspy').scrollSpy({"scrollOffset": 20});
     
-    var countdownSeconds = 1509753600 - (new Date).getTime()/1000;
-    var clock = $('#countdown').FlipClock(countdownSeconds, {
-		clockFace: 'DailyCounter',
-        countdown: true
-    });
-
-    $('.flip-clock-label').remove();
-    $('.flip-clock-wrapper').css('margin','0');
     $('#tubes-row').css('height', $(document).width()/maxWidth*bannerHeight + 'px');
     var scaleValue = Math.sqrt(startScale*$(window).width()/maxWidth);
-    if(!iOS) {
-        $('#countdown').css('zoom', scaleValue);
-        $('#countdown').css('transform','scale(' + scaleValue + ')');
-        var tubeHeight = $('#tubes').height();
-        var ratio = $(window).height()/$(window).width();
-        var heightScale = ratio > breakpoint ? longScale : shortScale;
-        $('#countdown-content').css('height', tubeHeight/heightScale);
-    }
-    else {
-        $('#countdown-content').remove();
-        $('#date').empty();
-        $('#tubes').css('background-image', "url('/static/assets/images/tubes-only4.png')");
-    }
+    // if(!iOS) {
+    //     $('#countdown').css('zoom', scaleValue);
+    //     $('#countdown').css('transform','scale(' + scaleValue + ')');
+    //     var tubeHeight = $('#tubes').height();
+    //     var ratio = $(window).height()/$(window).width();
+    //     var heightScale = ratio > breakpoint ? longScale : shortScale;
+    //     $('#countdown-content').css('height', tubeHeight/heightScale);
+    // }
+    $('#countdown-content').remove();
+    $('#date').empty();
+    $('#tubes').css('background-image', "url('/static/assets/images/tubes-only4.png')");
 
     var navtop = $('#navbar').offset().top;
     var navbar = document.getElementById('navbar');
